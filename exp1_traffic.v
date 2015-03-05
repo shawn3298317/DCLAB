32c68188682e0447b6ed05cbefabf7702e9f20d3
shawn <shawn3298317lin@gmail.com>
2015-03-05 13:31:26 +0800

    added exp1_traffic.v

---
+++ exp1_traffic.v
@@ -0,0 +1,31 @@
+//*************************************************************************************************//
+// File: exp1_traffic.v                                                                            //
+// Description: This is the verilog file for traffic light control logic                           //
+//*************************************************************************************************//
+
+
+
+//*  
+// @parameter clk   : 50MHz clock signal
+// @parameter reset : reset signal
+// @parameter change: change signal
+//*
+module traffic_ctr (
+	clk,
+	reset,
+	change
+	);
+	
+
+//*******combinational part*********//
+
+
+
+
+
+//*******sequential part************//
+
+	
+	
+endmodule
+
