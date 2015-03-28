c53a53c500b33c65e0800ce18d7709a88e3a2f71
shawn <shawn3298317lin@gmail.com>
2015-03-28 20:31:43 +0800

    not_completed

--- exp1_traffic.v
+++ exp1_traffic.v
@@ -1,6 +1,7 @@
 //*************************************************************************************************//
 // File: exp1_traffic.v                                                                            //
 // Description: This is the verilog file for traffic light control logic                           //
+//              phase count can be customized by signal                                            //
 //*************************************************************************************************//
 
 
@@ -13,13 +14,41 @@
 module traffic_ctr (
 	clk,
 	reset,
-	change
+	change,
+	HEX0,
+	HEX1
 	);
-	
+//==== input/output declaration======//
+	//--- input ---//
+	input clk;
+	input reset;
+	input change;
+
+	//--- output ---//
+	output [6:0] HEX0;
+	output [6:0] HEX1;
+
+//==== reg/wire declaration =========//
+	//--- output ---//
+	reg [6:0] HEX0;
+	reg [6:0] HEX1;
+
+	//--- wires ---//
+	wire clk_16;
+	wire [23:0] next_clks;
+	reg  [4:0]  next_countdown;
+	reg  [6:0]  next_HEX0;
+	reg  [6:0]  next_HEX1;
+
+
+
 
 //*******combinational part*********//
+	
 
 
+
+	
 
 
 
